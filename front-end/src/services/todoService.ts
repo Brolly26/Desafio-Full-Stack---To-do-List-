7b090ac814634dc4d31b3260829980e6d246a3bc
GabrielCode <bielaugusto8@hotmail.com>
2025-03-28 23:53:40 -0300

    Atualizando o servidor

--- front-end/src/services/todoService.ts
+++ front-end/src/services/todoService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/todos"
+  baseURL: "https://desafio-full-stack-to-do-list-production.up.railway.app/todos"
 });
 
 export interface Todo {
