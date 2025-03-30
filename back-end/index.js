b207f62c9e30b439c9ad5a2ebcf366af9bc28bfb
GabrielCode <bielaugusto8@hotmail.com>
2025-03-29 22:18:12 -0300

    Atualização port

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import todasRotas from "./routes/allRoutes.js";
 
-const PORT = process.env.PORT || 3000 || 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors());
